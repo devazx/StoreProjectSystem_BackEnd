71d0eeaf7dff518c33063516488491ec685c9c5a
Daniel Vaz <danielvaz.tzk@hotmail.com>
2025-04-09 20:18:33 -0300

    Input at a class trade for connection Users with Products

---
+++ StoreProjectSystem_BackEnd/StoreProjectSystem_BackEnd/Models/Trade.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreProjectSystem_BackEnd.Models
+{
+    public class Trade
+    {
+        public int ProductId { get; set; }
+        public EndProduct EndProduct { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
