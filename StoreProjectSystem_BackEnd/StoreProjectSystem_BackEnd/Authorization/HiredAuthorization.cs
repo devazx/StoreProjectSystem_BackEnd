62bb33b6271a05939af7cbc0831561f6dd21922b
Daniel Vaz <danielvaz.tzk@hotmail.com>
2025-04-29 22:32:09 -0300

    Create a Logic of Requiriment for authorization

--- StoreProjectSystem_BackEnd/StoreProjectSystem_BackEnd/Authorization/HiredAuthorization.cs
+++ StoreProjectSystem_BackEnd/StoreProjectSystem_BackEnd/Authorization/HiredAuthorization.cs
@@ -7,6 +7,18 @@ namespace StoreProjectSystem_BackEnd.Authorization
     {
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, Employee requirement)
         {
+            var Hired = context.User.FindFirst(claim => claim.Type == "Hired");
+
+            if (Hired == null) return Task.CompletedTask;
+
+            if (bool.TryParse(Hired.Value, out bool isHired))
+            {
+                if (isHired == requirement.Hired) 
+                {                 
+                    context.Succeed(requirement);
+                }
+            }
+
             return Task.CompletedTask;
         }
     }
